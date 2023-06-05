c5995e8291e2b9a4e9c9e082656f8df3fc995a44
jhintmain <jhintmain@gmail.com>
2023-06-06 02:59:49 +0900

    스프링부트와 JPA 활용1 - 환경셋팅

---
+++ NOTE.md
@@ -0,0 +1,12 @@
+# lombok 셋팅
+1. setting > plugin > lombok install
+2. setting > build , execution, development > Annotation Processer> enable annnotation 체크
+
+# 서버 안끄고 재컴파일만으로 수정 사항 적용하기
+implementation 'org.springframework.boot:spring-boot-devtools'
+
+# Ctrl + Alt + V > return 형으로 데이터 변수 만들어줌
+
+# application.properties 나 application.yml 둘중하나만 사용하면됨
+
+# test에서  tdd + tab 하면 테스트 코드 메소드 형식 생성
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("member1");
+
+        // when
+        Long id = memberRepository.save(member);
+        Member member1 = memberRepository.find(id);
+
+        // then
+        assertThat(member.getId()).isEqualTo(member1.getId());
+        assertThat(member.getUsername()).isEqualTo(member1.getUsername());
+        assertThat(member).isEqualTo(member1);
+        System.out.println(member == member1);
+
+    }
+}
